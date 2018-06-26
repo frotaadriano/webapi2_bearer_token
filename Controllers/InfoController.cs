596d2cac91b355088bd09e6dcb3ac3144a22eefd
ADRIANO FROTA <adriano.internet@gmail.com>
2018-06-26 00:53:19 -0300

    First commit
    First commit

---
+++ Controllers/InfoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI_withTokenProject.Controllers
+{
+    [RoutePrefix("info")]
+    public class InfoController : ApiController
+    {
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            var info = "Token Based Authentication using ASP.NET Web API 2, Owin, and Identity: http://bitoftech.net/2014/06/01/token-based-authentication-asp-net-web-api-2-owin-asp-net-identity/"; 
+            return Ok(info);
+        }
+
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebAPI_withTokenProject.Providers;
+
+[assembly: OwinStartup(typeof(WebAPI_withTokenProject.Startup))]
+namespace WebAPI_withTokenProject
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureOAuth(app);
+
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll); // step 11
+            app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
+    }
+}
